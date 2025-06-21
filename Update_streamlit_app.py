812fb933b1ab1e9b6a58ae0d9389a5a97c782cec
Sathiyanantham84 <tvkinstitute.courses@gmail.com>
2025-06-21 22:28:45 +0530

    Update Update_streamlit_app.py

--- Update_streamlit_app.py
+++ Update_streamlit_app.py
@@ -33,7 +33,7 @@ if Ingredients_list:
     Ingredients_string = ''
 
     for fruit_chosen in Ingredients_list:
-        Ingredients_string += fruit_chosen + '  '
+        Ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
